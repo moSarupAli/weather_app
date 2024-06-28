2c4a2f8b120a53a7c323e3803ca510dab4a728b4
moSarupAli <sarupmondal549@gmail.com>
2024-06-28 15:58:03 +0530

    first commit

---
+++ dist/js/dataFunctions.js
@@ -0,0 +1,52 @@
+// const WEATHER_API_KEY = '98001b129748438090aed2ce8d0edf5c';
+
+export const setLocationObject = (locationObj, coordsObj) => {
+    const { lat, lon, name, unit } = coordsObj;
+    locationObj.setLat(lat);
+    locationObj.setLon(lon);
+    locationObj.setName(name);
+    if(unit) {
+        locationObj.setUnit(unit);
+    }
+};
+
+export const getHomeLocation = () => {
+    return localStorage.getItem("defaultWeatherLocation");
+};
+
+export const getWeatherFromCoords = async (locationObj) => {
+    const lat = locationObj.getLat();
+    const lon = locationObj.getLon();
+    const units = locationObj.getUnit();
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+    try {
+        const weatherStream = await fetch(url);
+        const weatherJson = await weatherStream.json();
+        return weatherJson;
+    }
+    catch (err) {
+        console.error(err);
+    }
+};
+
+export const getCoordsFromApi = async (entryText, units) => {
+    const regex = /^\d+$/g;
+    const flag = regex.test(entryText) ? "zip" : "q";
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+    const encodedUrl = encodeURI(url);
+    try{
+        const dataStream = await fetch(encodedUrl);
+        const jsonData = await dataStream.json();
+        console.log(jsonData);
+        return jsonData;
+    }
+    catch (err) {
+        console.error(err.stack);
+    }
+}
+
+export const cleanText = (text) => {
+    const regex = / {2,}/g;
+    const entryText = text.replaceAll(regex, " ").trim();
+    return entryText;
+};
